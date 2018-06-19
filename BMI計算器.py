a7872ed7906c3377939bc9d080c61a7bfa7f69af
迪 安 <andypei840126@gmail.com>
2018-06-20 00:03:06 +0800

    1.BMI計算

---
+++ BMI計算器.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+Hight_people = int(input("你的身高(cm)? :"))
+Weight_people = int(input("你的體重(kg)? :"))
+
+BMI = Weight_people/((Hight_people/100)**2)
+if(BMI>=28):health = "肥胖"
+elif(BMI>=24):
+    health = "超重"
+elif(BMI>=18.5):
+    health = "正常體重"
+else:
+    health = "過輕"
+print("你的BMI值為%.2f,%s"%(BMI,health))
+input("按任意建後結束...")
